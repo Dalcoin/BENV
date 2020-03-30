82579629d9d3f2e16df0bd7bab0ff8bc002c21eb
Dolcwan <51341410+Dolcwan@users.noreply.github.com>
2020-03-30 00:54:54 -0700

    major revisions

--- exe.py
+++ exe.py
@@ -4,6 +4,7 @@ import os
 from pmod import benv               # 'benv' is not a standard pmod module and must be added
 from pmod import ioparse as iop
 from pmod import cmdline as cml 
+from pmod import mathops as mops
 
 skval, eos = True, True
 
@@ -13,13 +14,7 @@ success, pwd = cmv.cmd('pwd')
 
 benv_instance = benv.benv()
 benvals = benv.benv_skval_loop(benv_instance)
+printlines = benv_instance.format_skval_benv_vals(benvals)
 
-outlines = []
-for i in xrange(len(benvals)):
-    if(i > 0):
-        outlines.append(str(benvals[i][0][0])+str(benvals[i][0][1])+'  '+str(benvals[i][1][0])+str(benvals[i][1][1]))
-    else:
-        outlines.append(benvals[i][0]+' '+benvals[i][1])
-
-iop.flat_file_write(cmv.pw_join(pwd,"results.srt"), outlines)
-cmv.cmd('mv results.srt benv_data')
+iop.flat_file_write(cmv.pw_join(pwd,"results.srt"), printlines)
+cmv.cmd('mv results.srt data')
