76e6ff5e500d7c5ca6b8acb12e0463a0faa38b96
Dolcwan <51341410+Dolcwan@users.noreply.github.com>
2020-04-01 02:43:27 -0700

    updates to execute script

--- exe.py
+++ exe.py
@@ -1,20 +1,29 @@
 import os
 
-
 from pmod import benv               # 'benv' is not a standard pmod module and must be added
 from pmod import ioparse as iop
 from pmod import cmdline as cml 
 from pmod import mathops as mops
 
-skval, eos = True, True
-
 cmv = cml.path_parse('linux')
 success, pwd = cmv.cmd('pwd')
 
+success, value = cmv.cmd('cd data')
+success, value = cmv.cmd('ls')
+if('results.srt' in value):
+    success, value = cmv.cmd('rm results.srt')    
+success, value = cmv.cmd('cd ..')
 
-benv_instance = benv.benv()
-benvals = benv.benv_skval_loop(benv_instance)
-printlines = benv_instance.format_skval_benv_vals(benvals)
+benv_inst = benv.benv()
+groups = benv_inst.benv_eos_loop()
 
-iop.flat_file_write(cmv.pw_join(pwd,"results.srt"), printlines)
+outlines = []
+outlines.append('\n')
+for i,j in groups:
+    outlines.append(j+"\n")
+    for k in i:
+        outlines.append('  '+k)
+    outlines.append('\n')
+
+iop.flat_file_write(cmv.pw_join(pwd,"results.srt"), outlines)
 cmv.cmd('mv results.srt data')
