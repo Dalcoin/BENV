c718f7be78bfe1943853a8be428ad313ad411a61
Dolcwan <51341410+Dolcwan@users.noreply.github.com>
2020-05-09 00:57:46 -0700

    Create compile_BENV.py

---
+++ compile_BENV.py
@@ -0,0 +1,20 @@
+import pmod.compile as comp
+
+'''
+This script faciliates compiling binaries from the source ('src') folder
+and moving those binaries to the binary folder ('bin'). This script uses
+the scaffolding provided by 'compile.py'
+'''
+
+binList = ("xeb_server", "aux")
+srcScript = "compile_benv.sh"
+binScript = "run.sh"
+
+success = comp.compileFunc(binList, src_script=srcScript, bin_script=binScript)
+
+print(" ")
+if(success):
+    print("No fatal errors detected, see above for runtime messesges")
+else:
+    print("Fatal error detected! See above for runtime error(s)")
+print(" ")
